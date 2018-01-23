7d43781c2d9b8ba4be352dcc2b9a6afc452f74f2
Marcus Horton <marcush456@gmail.com>
2018-01-22 22:09:30 -0600

    overhauled most everything

--- mean_reverting_strat1.py
+++ mean_reverting_strat1.py
@@ -5,7 +5,6 @@ with open("api_key.json", 'r') as fp:
 	api_key = json.load(fp)
 
 default_version = "v1.1"
-
 market = "ETH-ADA"
 price_delta = .000004
 max_quantity = 100
@@ -43,32 +42,26 @@ new_orders = btrx.get_open_orders(market=market)["result"]
 orders = [order for order in new_orders if order["OrderUuid"] not in ignore_orders]
 order_uuids = [order["OrderUuid"] for order in orders]
 
-# exit condition
+# make sure we have our orders
+if len(order_uuids) != 2 * n_levels:
+	print("WARNING: got " + str(len(order_uuids)) + " orders. Expected " + str(2*n_levels)+".")
+	print("Aborting before entering strategy...")
+	for order in orders:
+		btrx.cancel(order["OrderUuid"])
+	assert False
+
+# initialize stuff
 n_failed_order_calls = 0
 n_iters = 0
+missing_orders = []
+missing_uuids = []
+new_orders = []
 
 while True:
 	
-	# request open order list, filter non-strategy orders
-	order_call = btrx.get_open_orders(market=market)
-	if order_call["success"]:
-		new_orders = [order for order in order_call["result"] if order["OrderUuid"] not in ignore_orders]
-		n_failed_order_calls = 0
-	else:
-		n_failed_order_calls += 1
-		if n_failed_order_calls > 10:
-			print("failed to retrieve orders 10 consecutive times... exiting strategy")
-			break
-		continue
-
-	# completed orders will be missing.
-	new_uuids = [order["OrderUuid"] for order in new_orders]
-	missing_uuids = [uuid for uuid in order_uuids if uuid not in new_uuids]
-	missing_orders = [order for order in orders if order["OrderUuid"] in missing_uuids]
-
-	current_ticker = btrx.get_ticker(market)["result"]
-
 	for order in missing_orders:
+
+		current_ticker = btrx.get_ticker(market)["result"]
 
 		if order["OrderType"] == "LIMIT_SELL":
 			print("Sell order filled:")
@@ -77,26 +70,34 @@ while True:
 							quantity=max_quantity//n_levels,
 							rate=min(current_ticker["Ask"], order["Limit"] - price_delta)
 				)
-		else:
+		elif order["OrderType"] == "LIMIT_BUY":
 			print("Buy order filled:")
 			print("price:", order["Limit"])
 			btrx.sell_limit(market=market,
 							quantity=max_quantity//n_levels,
 							rate=max(current_ticker["Bid"], order["Limit"] + price_delta)
 				)
+		else:
+			print("Got unexpected OrderType! : " + order["OrderType"])
 
 
-	if len(missing_orders) > 0:
-		new_orders = btrx.get_open_orders(market=market)["result"]
-		orders = [order for order in new_orders if order["OrderUuid"] not in ignore_orders]
-		order_uuids = [order["OrderUuid"] for order in orders]
+	while len(new_orders) != len(missing_orders):
+		new_order_call = btrx.get_open_orders(market=market)
+		new_orders = [order for order in new_order_call["result"] if order["OrderUuid"] not in ignore_orders
+																	and order["OrderUuid"] not in order_uuids]
+
+	orders = [order for order in orders if order["OrderUuid"] not in missing_uuids]
+	orders += new_orders
+	order_uuids = [order["OrderUuid"] for order in orders] 
+	assert len(orders) == 2 * n_levels
 
 	btrx.wait()
 
-	#assert len(order_uuids) == 2 * n_levels, "Number of strategy orders is off... got " + str(len(order_uuids))
-	if len(order_uuids) != 2 * n_levels: 
-		print("WARNING: got " + str(len(order_uuids)) + " orders. Expected " + str(2*n_levels)+".")
-		print("number of missing orders found:", len(missing_orders))
+	new_order_call = btrx.get_open_orders(market=market)
+	new_orders = [order for order in new_order_call["result"] if order["OrderUuid"] not in ignore_orders]
+	new_order_uuids = [order["OrderUuid"] for order in new_orders]
+	missing_uuids = [uuid for uuid in order_uuids if uuid not in new_order_uuids]
+	missing_orders = [order for order in orders if order["OrderUuid"] in missing_uuids]
 
 	n_iters += 1
 	if n_iters % display_freq == 0:
@@ -109,6 +110,7 @@ while True:
 	if n_iters > max_iters:
 		break
 
+	new_orders = []
 
 for order in orders:
 	btrx.cancel(order["OrderUuid"])
