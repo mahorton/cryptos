5fc8b8dff89265cd929ea339e7773df490c0bb0b
Marcus Horton <marcush456@gmail.com>
2018-01-23 20:06:32 -0600

    fixed profit counting

--- mean_reverting_strat1.py
+++ mean_reverting_strat1.py
@@ -21,7 +21,7 @@ btrx = bittrex.Bittrex(api_key=api_key["api_key"],
 					   calls_per_second=1/60, 
 					   api_version=default_version)
 
-
+level_quantity = max_quantity//n_levels
 # get existing orders to be ignored by strategy
 existing_orders = btrx.get_open_orders(market=market)["result"]
 ignore_orders = [x["OrderUuid"] for x in existing_orders]
@@ -29,19 +29,19 @@ ignore_orders = [x["OrderUuid"] for x in existing_orders]
 ticker = btrx.get_ticker(market)["result"]
 initial_price = ticker["Last"]
 
-profit_per_flip = price_delta * (max_quantity//n_levels) - (.005*(max_quantity//n_levels)*initial_price)
+profit_per_flip = price_delta * (level_quantity) - (.005*(level_quantity)*initial_price)
 print("Approximate profit per flip: "+str(profit_per_flip)+" "+market.split("-")[0]+".")
 
 
 # place initial orders
 for i in range(n_levels):
 	btrx.buy_limit(market=market,
-					quantity=max_quantity//n_levels,
+					quantity=level_quantity,
 					rate=min(ticker["Ask"], initial_price - (n_levels-i)*price_delta)
 		)
 
 	btrx.sell_limit(market=market,
-					quantity=max_quantity//n_levels,
+					quantity=level_quantity,
 					rate=max(ticker["Bid"], initial_price + (n_levels-i)*price_delta)
 		)
 	ticker = btrx.get_ticker(market)["result"]
@@ -70,6 +70,7 @@ n_buy_orders = n_levels
 n_sell_orders = n_levels
 majority_order = None
 n_flips = 0
+profit = 0
 
 while True:
 	for order in missing_orders:
@@ -79,23 +80,28 @@ while True:
 			print("Sell order filled:")
 			print("price:", order["Limit"])
 			btrx.buy_limit(market=market,
-							quantity=max_quantity//n_levels,
+							quantity=level_quantity,
 							rate=min(current_ticker["Ask"], order["Limit"] - price_delta)
 				)
 			n_sell_orders -= 1
 			n_buy_orders += 1
-			if majority_order == "LIMIT_BUY": n_flips += 1
+			if majority_order == "LIMIT_BUY": 
+				n_flips += 1
+				profit += price_delta*level_quantity - .0025*level_quantity*(2*order["Limit"]-price_delta)
 
 		elif order["OrderType"] == "LIMIT_BUY":
 			print("Buy order filled:")
 			print("price:", order["Limit"])
 			btrx.sell_limit(market=market,
-							quantity=max_quantity//n_levels,
+							quantity=level_quantity,
 							rate=max(current_ticker["Bid"], order["Limit"] + price_delta)
 				)
 			n_sell_orders += 1
 			n_buy_orders -= 1
-			if majority_order == "LIMIT_SELL": n_flips += 1
+			if majority_order == "LIMIT_SELL": 
+				n_flips += 1
+				profit += price_delta*level_quantity - .0025*level_quantity*(2*order["Limit"]+price_delta)
+
 		else:
 			print("Got unexpected OrderType! : " + order["OrderType"])
 		
@@ -134,7 +140,7 @@ while True:
 	if n_iters % display_freq == 0:
 		print("Completed iteration " + str(n_iters))
 		print("number of successful flips:", n_flips)
-		print("resulting profit:", n_flips * profit_per_flip)
+		print("resulting profit:", profit)
 		print("remaining orders:")
 		order_prices = sorted([order["Limit"] for order in orders])
 		print(order_prices)
